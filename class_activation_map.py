c9ee351df0afd96da8d3d9ade9934d4cbcd5122e
Yadong Zhang <43513009+ydup@users.noreply.github.com>
2019-07-01 12:22:42 +0800

    Update class_activation_map.py
    Fix batch_matmul with matmul and fix the shape of label_w

--- class_activation_map.py
+++ class_activation_map.py
@@ -10,9 +10,9 @@ def get_class_map(label, conv, im_width):
     conv_resized = tf.image.resize_bilinear(conv, [im_width, im_width])
     with tf.variable_scope('LeNet/GAP', reuse=True):
         label_w = tf.gather(tf.transpose(tf.get_variable('W')), label)
-        label_w = tf.reshape(label_w, [-1, output_channels, 1])
+        label_w = tf.reshape(label_w, [-1, 1, output_channels])
     conv_resized = tf.reshape(conv_resized, [-1, im_width * im_width, output_channels])
-    classmap = tf.batch_matmul(conv_resized, label_w)
+    classmap = tf.matmul(conv_resized, label_w, adjoint_b=True)
     classmap = tf.reshape(classmap, [-1, im_width, im_width])
     return classmap
 
